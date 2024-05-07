160c4af4f515644ecb3ede36918c97b3767773b6
Raven Glenn Lagmay <137616076+aconite12@users.noreply.github.com>
2024-05-07 18:45:56 +0800

    Update ViewCart.js

--- src/Components/ViewCart.js
+++ src/Components/ViewCart.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ProductInformation from './ProductInformation';
 import ButtonComponent from './ButtonComponent';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const ViewCart = ({ addedItems, handleDelete, handleClearCart}) => {
+const ViewCart = ({ handleDelete, handleClearCart }) => {
+  const [addedItems, setAddedItems] = useState([]);
+
+  useEffect(() => {
+    fetchCartItems();
+  }, []);
+
+  const fetchCartItems = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/cart');
+      const data = await response.json();
+      console.log('Cart Items Response:', data);
+      if (response.status === 200) {
+        setAddedItems(data.cartItems);
+      } else {
+        throw new Error('Failed to fetch cart items');
+      }
+    } catch (error) {
+      console.error('Error fetching cart items:', error);
+    }
+  };
+
   return (
     <div className='Product'>
       <h1>My Cart</h1>
@@ -14,27 +35,23 @@ const ViewCart = ({ addedItems, handleDelete, handleClearCart}) => {
           <div key={index}>
             <ul>
               <li key={index}>
-              <ProductInformation name={item.name} price={item.price} />
-              <ButtonComponent className='btn btn-primary btn-danger' buttonName={"Delete Item"} onClickFunction={() => handleDelete(index)}/>
-
+                <ProductInformation productName={item.productName} price={item.price} />
+                <ButtonComponent className='btn btn-primary btn-danger' buttonName={"Delete Item"} onClickFunction={() => handleDelete(index)} />
               </li>
             </ul>
           </div>
         ))
       )}
-       <div>
-       <nav>
+      <div>
+        <nav>
           <ul>
-        
-        <Link to={'/ProductPage'}><ButtonComponent className='btn btn-primary' buttonName={"Check Out"} onClickFunction={handleClearCart}/></Link>
-         <br/>
-          <Link to="/ProductPage"> <ButtonComponent className='btn btn-primary' buttonName={"Product Page"}></ButtonComponent></Link>
+            <Link to={'/ProductPage'}><ButtonComponent className='btn btn-primary' buttonName={"Check Out"} onClickFunction={handleClearCart} /></Link>
+            <br />
+            <Link to="/ProductPage"> <ButtonComponent className='btn btn-primary' buttonName={"Product Page"}></ButtonComponent></Link>
           </ul>
         </nav>
-
       </div>
     </div>
-    
   );
 };
 
