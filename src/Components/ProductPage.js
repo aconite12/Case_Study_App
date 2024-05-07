d3a40ba6f61511710aba1f4fb48da6dde69e03e6
Raven Glenn Lagmay <137616076+aconite12@users.noreply.github.com>
2024-05-07 18:45:12 +0800

    Update ProductPage.js

--- src/Components/ProductPage.js
+++ src/Components/ProductPage.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, { useState } from 'react'; // Import useState hook
+import {Link} from 'react-router-dom';
 import ProductInformation from './ProductInformation';
 import ButtonComponent from './ButtonComponent';
 
 const Product = ({ products, onAddToCart }) => {
+
+  const [addedItems, setAddedItems] = useState([]);
+
+  const handleAddToCart = async (product) => {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/cart/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          productName: product.productName,
+          price: product.price,
+          productDescription: product.productDescription
+        }),
+      });
+      if (!response.ok) {
+        throw new Error('Failed to add product to cart');
+      }
+      const data = await response.json();
+      console.log('Product added to cart:', data);
+  
+      setAddedItems(prevItems => [...prevItems, product]);
+  
+    } catch (error) {
+      console.error('Error adding product to cart:', error);
+    }
+  }; 
+
   return (
     <div className='Product'>
       <div className="grid-container">
@@ -15,7 +44,7 @@ const Product = ({ products, onAddToCart }) => {
                 <ButtonComponent
                   className='btn btn-primary'
                   buttonName='Add To Cart'
-                  onClickFunction={() => onAddToCart(product)}
+                  onClickFunction={() => handleAddToCart(product)}
                 />
               </div>
             </div>
