0661b156e9161ba05f945f7ebe9cf3aa3b9ab205
Raven Glenn Lagmay <137616076+aconite12@users.noreply.github.com>
2024-05-08 12:09:39 +0800

    Update ProductPage.js

--- src/Components/ProductPage.js
+++ src/Components/ProductPage.js
@@ -1,35 +1,60 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, { useState } from 'react'; // Import useState hook
+import {Link} from 'react-router-dom';
 import ProductInformation from './ProductInformation';
 import ButtonComponent from './ButtonComponent';
 
 const Product = ({ products, onAddToCart }) => {
+
+  const [addedItems, setAddedItems] = useState([]);
+
+  const handleAddToCart = async (product) => {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/cart/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          productName: product.productName,
+          price: product.price,
+          productDescription: product.productDescription
+        }),
+      });
+      if (!response.ok) {
+        throw new Error('Failed to add product to cart');
+      }
+      const data = await response.json();
+  
+      setAddedItems(prevItems => [...prevItems, product]);
+      onAddToCart();
+    } catch (error) {
+      console.error('Error adding product to cart:', error);
+    }
+  }; 
+
   return (
     <div className='Product'>
       <div className="grid-container">
-        {products.map((product, index) => (
-          <div className="container">
-            <div key={index} className="card">
+        {products.map((product) => (
+          <div key={product.id} className="container">
+            <div className="card">
               <div className="card-body">
                 <ProductInformation {...product} />
                 <ButtonComponent
                   className='btn btn-primary'
                   buttonName='Add To Cart'
-                  onClickFunction={() => onAddToCart(product)}
+                  onClickFunction={() => handleAddToCart(product)}
                 />
               </div>
             </div>
-
           </div>
         ))}
       </div>
       <br/><br/>
       <div>
-        
         <nav>
           <ul>
           <Link to="/ViewCart"> <ButtonComponent className='btn btn-primary' buttonName={"View Cart"}></ButtonComponent></Link>
-         
           </ul>
         </nav>
       </div>
