a55adf52ce6f0d265fae016a363311baf5ff6696
Raven Glenn Lagmay <137616076+aconite12@users.noreply.github.com>
2024-05-08 10:58:18 +0800

    Update ViewCart.js

--- src/Components/ViewCart.js
+++ src/Components/ViewCart.js
@@ -2,54 +2,41 @@ import React, { useState, useEffect } from 'react';
 import ProductInformation from './ProductInformation';
 import ButtonComponent from './ButtonComponent';
 import { Link } from 'react-router-dom';
+import '../../src/App.css';
 
-const ViewCart = ({ handleDelete, handleClearCart }) => {
-  const [addedItems, setAddedItems] = useState([]);
-
-  useEffect(() => {
-    fetchCartItems();
-  }, []);
-
-  const fetchCartItems = async () => {
-    try {
-      const response = await fetch('http://127.0.0.1:8000/api/cart');
-      const data = await response.json();
-      console.log('Cart Items Response:', data);
-      if (response.status === 200) {
-        setAddedItems(data.cartItems);
-      } else {
-        throw new Error('Failed to fetch cart items');
-      }
-    } catch (error) {
-      console.error('Error fetching cart items:', error);
-    }
-  };
-
+const ViewCart = ({ cartItems, handleDelete, handleClearCart }) => {
   return (
     <div className='Product'>
-      <h1>My Cart</h1>
-      {addedItems.length === 0 ? (
-        <p>No items added to the cart</p>
-      ) : (
-        addedItems.map((item, index) => (
-          <div key={index}>
-            <ul>
-              <li key={index}>
-                <ProductInformation productName={item.productName} price={item.price} />
-                <ButtonComponent className='btn btn-primary btn-danger' buttonName={"Delete Item"} onClickFunction={() => handleDelete(index)} />
-              </li>
-            </ul>
+      <h1 className="cart-title">My Cart</h1>
+      <div className="cart-container">
+        {cartItems.length === 0 ? (
+          <p>No items added to the cart</p>
+        ) : (
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Item</th>
+                  <th>Price</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItems.map(item => (
+                  <tr key={item.id}>
+                    <td>{item.productName}</td>
+                    <td>{item.price}</td>
+                    <td>
+                      <ButtonComponent className='btn btn-primary btn-danger' buttonName={"Delete Item"} onClickFunction={() => handleDelete(item.id)} />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <ButtonComponent className='btn btn-primary' buttonName={"Check Out"} onClickFunction={handleClearCart} />
+            <Link to="/ProductPage"><ButtonComponent className='btn btn-primary' buttonName={"Product Page"}></ButtonComponent></Link>
           </div>
-        ))
-      )}
-      <div>
-        <nav>
-          <ul>
-            <Link to={'/ProductPage'}><ButtonComponent className='btn btn-primary' buttonName={"Check Out"} onClickFunction={handleClearCart} /></Link>
-            <br />
-            <Link to="/ProductPage"> <ButtonComponent className='btn btn-primary' buttonName={"Product Page"}></ButtonComponent></Link>
-          </ul>
-        </nav>
+        )}
       </div>
     </div>
   );
